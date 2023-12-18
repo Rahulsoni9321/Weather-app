eed82897a2a7f7a38f9ad0658f42688002078530
Rahulsoni3108 <90192605+Rahulsoni3108@users.noreply.github.com>
2023-12-19 00:56:32 +0530

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apikey="a73dce1fe0f40bc3a9e28e54e56a6d57";
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const inputtext=document.querySelector(".first input");
 const searchbtn=document.querySelector(".first button");
-const weatherimage=document.querySelector(".weatherimage");
+const image=document.querySelector(".images");
 document.querySelector(".information").style.display="none";
 // checkWeather(bengaluru);
 searchbtn.addEventListener("click",()=>{checkWeather(inputtext.value)});
@@ -16,36 +16,36 @@ async function checkWeather(city){
      
     else {
     let data=await response.json();
-    
     console.log(data);
     document.querySelector(".error").style.display="none";
-        document.querySelector(".information").style.display="block";
+    document.querySelector(".information").style.display="block";
     
-  
+    if (data.weather[0].main==="Clear"){
+        image.src="images/clear.png";
+    }
+    else if (data.weather[0].main==="Clouds"){
+       image.src="images/clouds.png";
+    }
+    else if (data.weather[0].main==="Mist"){
+        image.src="images/mist.png";
+    }
+    else if (data.weather[0].main==="Snow"){
+       image.src="images/snow.png";
+    }
+    else if (data.weather[0].main==="Rain"){
+       image.src="images/rain.png";
+    }
+    else if (data.weather[0].main==="Drizzle"){
+        image.src="images/drizzle.png";
+    }
+    else 
+    image.src="images/clear.png";
+    
     document.querySelector(".temps").innerHTML=Math.round(data.main.temp) + "°c";
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".humids").innerHTML=data.main.humidity+"%";
-    // document.querySelector(".value").innerHTML=data.weather[0].main;
     document.querySelector(".speeds").innerHTML=data.wind.speed+"km/h";
 
-    if (data.weather[0].main==="Clear"){
-        weatherimage.src="images/clear.png";
-    }
-    else if (data.weather.main==="Clouds"){
-        weatherimage.src="clouds.png";
-    }
-    else if (data.weather[0].main==="Mist"){
-        weatherimage.src="images/mist.png";
-    }
-    else if (data.weather[0].main==="Snow"){
-        weatherimage.src="images/snow.png";
-    }
-    else if (data.weather[0].main==="Rain"){
-        weatherimage.src="images/rain.png";
-    }
-    else if (data.weather[0].main==="Drizzle"){
-        weatherimage.src="images/drizzle.png";
-    }
     
 }
 
